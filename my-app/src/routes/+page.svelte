e9843d528a462611c18436daf722c0cc11bf534f
wearestat <159584962+wearestat@users.noreply.github.com>
2026-03-24 12:22:27 +0000

    Changed button to button group and centered div

--- my-app/src/routes/+page.svelte
+++ my-app/src/routes/+page.svelte
@@ -1,5 +1,13 @@
 <script>
+ import ArrowUpIcon from "@lucide/svelte/icons/arrow-up";
  import { Button } from "$lib/components/ui/button/index.js";
 </script>
- 
-<Button>Click me</Button>
+
+<div class="flex h-screen items-center justify-center">
+ <div class="flex gap-2">
+  <Button variant="outline">Button</Button>
+  <Button variant="outline" size="icon" aria-label="Submit">
+   <ArrowUpIcon />
+  </Button>
+ </div>
+</div>
